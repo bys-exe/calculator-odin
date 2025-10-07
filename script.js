6a4b19d36939cb0abe88179b4f0f1e24d5cf7003
bys_exe <batchuyugasai@gmail.com>
2025-10-07 19:02:40 +0530

    Create arithmetic operation functions
    created function for all operations and put them inside the operate function

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log('hello world');
+function add(a,b){
+    sum = a+b;
+    return sum;
+}
+function subtract(a,b){
+    subtract = a-b;
+    return subtract;
+}
+function multiply(a,b){
+    multiply = a*b;
+    return multiply;
+}
+function divide(a,b){
+    divide = a/b;
+    return divide;
+}
+let value1;
+let value2;
+let operation;
+function operate(value1, value2, operation){
+    if(operation == '+'){
+        return add(value1, value2);
+    }else if(operation == '-'){
+        return subtract(value1, value2);
+    }else if(operation == '*'){
+        return multiply(value1, value2);
+    }else if(operation == '/'){
+        return divide(value1, value2);
+    }
+}
