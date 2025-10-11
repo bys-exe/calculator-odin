209aa0a5199b5b0084c8dd8bf99e169b842779e9
bys_exe <batchuyugasai@gmail.com>
2025-10-11 11:38:30 +0530

    Change website look
    used css on the website and made it look good

--- script.js
+++ script.js
@@ -1,74 +1,76 @@
-function add(a,b){
-    result = a+b;
-    return result;
+function add(a, b) {
+  result = a + b;
+  return result;
 }
-function subtract(a,b){
-    result = a-b;
-    return result;
+function subtract(a, b) {
+  result = a - b;
+  return result;
 }
-function multiply(a,b){
-    result = a*b;
-    return result;
+function multiply(a, b) {
+  result = a * b;
+  return result;
 }
-function divide(a,b){
-    result = a/b;
-    return result;
-}
-
-function operate(value1, value2, operation){
-    if(operation == '+'){
-        return add(value1, value2);
-    }else if(operation == '-'){
-        return subtract(value1, value2);
-    }else if(operation == '*'){
-        return multiply(value1, value2);
-    }else if(operation == '/'){
-        return divide(value1, value2);
-    }
+function divide(a, b) {
+  result = a / b;
+  return result;
 }
 
-const buttons = document.querySelectorAll('button');
-const display = document.getElementById('display');
+function operate(value1, value2, operation) {
+  if (operation == "+") {
+    return add(value1, value2);
+  } else if (operation == "-") {
+    return subtract(value1, value2);
+  } else if (operation == "*") {
+    return multiply(value1, value2);
+  } else if (operation == "/") {
+    return divide(value1, value2);
+  }
+}
+
+const buttons = document.querySelectorAll("button");
+const display = document.getElementById("display");
 
 value1 = null;
 value2 = null;
 operation = null;
 
-for(let button of buttons){
-    button.addEventListener('click',()=>{
-        let btnText = button.innerText;
-        if (!isNaN(btnText)){
-            if (operation === null){
-                value1 = (value1 === null || value1 === undefined ? "" : value1) + btnText;
-                display.innerHTML = value1;
-            }else{
-                value2 = (value2 === null || value2 === undefined? "" : value2) + btnText;
-                display.innerHTML = value1 + ' ' + operation + ' ' + value2;
-            }
-        }else if(['+','-','*','/'].includes(btnText)){
-            if(value1 != null && value2 != null){
-                let result = operate(parseInt(value1),parseInt(value2),operation)
-                value1 = result;
-                value2 = null;
-                display.innerHTML = result;
-            }
-            operation = btnText;
-            display.innerHTML = ' '+ operation;
-        }else if(btnText === '='){
-            if(value1 !== null && value2 !== null && operation !== null){
-                let result = operate(parseInt(value1),parseInt(value2),operation);
-                display.innerHTML = result;
-                value1 = result;
-                value2 = null;
-                operation = null;
-            }else{
-                display.innerHTML = value1 !== null?value1 : 0;
-            }
-        }else if(btnText === 'C'){
-            value1 = null;
-            value2 = null;
-            operation = null;
-            display.innerHTML = '';
-        }
-    })
+for (let button of buttons) {
+  button.addEventListener("click", () => {
+    let btnText = button.innerText;
+    if (!isNaN(btnText)) {
+      if (operation === null) {
+        value1 =
+          (value1 === null || value1 === undefined ? "" : value1) + btnText;
+        display.innerHTML = value1;
+      } else {
+        value2 =
+          (value2 === null || value2 === undefined ? "" : value2) + btnText;
+        display.innerHTML = value1 + " " + operation + " " + value2;
+      }
+    } else if (["+", "-", "*", "/"].includes(btnText)) {
+      if (value1 != null && value2 != null) {
+        let result = operate(parseInt(value1), parseInt(value2), operation);
+        value1 = result;
+        value2 = null;
+        display.innerHTML = result;
+      }
+      operation = btnText;
+      display.innerHTML += " " + operation;
+    } else if (btnText === "=") {
+      if (value1 !== null && value2 !== null && operation !== null) {
+        let result = operate(parseInt(value1), parseInt(value2), operation);
+        display.innerHTML = result;
+        value1 = result;
+        value2 = null;
+        operation = null;
+      } else {
+        display.innerHTML = value1 !== null ? value1 : 0;
+      }
+    } else if (btnText === "C") {
+      value1 = null;
+      value2 = null;
+      operation = null;
+      display.innerHTML = "";
+    }
+  });
 }
